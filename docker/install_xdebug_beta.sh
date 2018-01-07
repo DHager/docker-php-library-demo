1705319acc117070861f707b12c7c954d6200454
Darien Hager <darien+git@technofovea.com>
2018-01-06 19:33:18 -0800

    Make Xdebug installation script more portable by using /bin/sh

--- docker/install_xdebug_beta.sh
+++ docker/install_xdebug_beta.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
 readonly VERSION=xdebug-2.6.0beta1
@@ -6,21 +6,23 @@ readonly SHA=49de661e1e18cbbd739fc9fb7a014a36f97a84d2c4a89417c358dd258340527d
 readonly URL=https://xdebug.org/files/${VERSION}.tgz
 
 
-pushd /tmp/;
-wget "${URL}";
+# Download, validate, and extract
+(
+    cd /tmp/
+    wget "${URL}";
+    echo "${SHA}"    "/tmp/${VERSION}.tgz" | sha256sum --check
+    tar -xzvf /tmp/${VERSION}.tgz;
+)
 
-# Validate checksum for security
-echo "${SHA}"    "/tmp/${VERSION}.tgz" | sha256sum --check
+# Installation steps
+(
+    cd /tmp/${VERSION}/;
+    phpize;
+    ./configure --enable-xdebug;
+    make clean;
+    make;
+    make install;
+)
 
-tar -xzvf /tmp/${VERSION}.tgz;
-cd ./${VERSION}/;
-
-phpize;
-./configure --enable-xdebug;
-make clean;
-make;
-make install;
-
-# Cleanup to avoid Docker image bloat
-popd
+# Clean up to avoid image bloat
 rm -fr /tmp/${VERSION}/;
